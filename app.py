6d50a0fc30e2014866afe3fbb7e41008cbeb42a9
OsmanElias <93014594+OsmanElias@users.noreply.github.com>
2023-12-28 10:35:52 -0500

    ChartData Update
    -Created endpoint to be able to fetch data for charts
    -Setup the logic for some frontend points

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
 from flask import Flask, jsonify
 from stock_utils import get_real_time_stock_data
+from stock_utils import get_stock_chart_data
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return "Hello, Flask!"
+    return "Hello !"
 
 @app.route('/api/stock/<symbol>') #Dynamic path for any stock symbol
 def stock_data(symbol):
     data = get_real_time_stock_data(symbol)
     return jsonify(data)
+
+@app.route('/api/stock/chartdata/<symbol>')
+def stock_chart_data(symbol):
+    chart_data = get_stock_chart_data(symbol)  # Function from stock_utils.py
+    return jsonify(chart_data)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- stock_utils.py
+++ stock_utils.py
@@ -9,3 +9,20 @@ def get_real_time_stock_data(symbol):
     url = f"https://www.alphavantage.co/query?function={function}&symbol={symbol}&interval={interval}&apikey={api_key}"
     response = requests.get(url)
     return response.json()
+
+def get_stock_chart_data(symbol):
+    api_key = os.getenv("STOCK_API_KEY")  # API Key
+    url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={api_key}&outputsize=compact"
+
+    response = requests.get(url)
+    data = response.json()
+
+    # Process and transform data into a format suitable for Chart.js
+    chart_data = []
+    for date, price_info in data['Time Series (Daily)'].items():
+        chart_data.append({
+            'date': date,
+            'price': float(price_info['4. close'])  # Adjust based on needed price type
+        })
+
+    return chart_data
